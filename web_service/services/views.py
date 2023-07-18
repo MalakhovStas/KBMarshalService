ac005e424fd96ee3b4d1c431beac497611cff429
MalakhovStas <ff9147782@gmail.com>
2023-07-19 00:29:56 +0300

    added some logic for file_verification

--- web_service/services/views.py
+++ web_service/services/views.py
@@ -1,20 +1,49 @@
-import time
-
-from django.views.generic import TemplateView
-from django.http import HttpResponseForbidden, HttpResponseRedirect
-from django.shortcuts import render
+from celery.result import AsyncResult
 from django.contrib import messages
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+
 from services.busness_logic import load_data_file
+from web_service.celery import app
 from web_service.settings import redis_cache
 
-from celery import current_task
-from celery_progress.backend import ProgressRecorder
-from celery_progress import tasks
-from celery.result import AsyncResult
+
+class BaseServicesPageView:
+    template_name = ""
+
+    def get_success_url(self):
+        pass
+
+    def get(self, request: WSGIRequest, *args, **kwargs):
+        if request.user.groups.filter(name__in=['users', 'admins']).exists() or request.user.is_superuser:
+            task = None
+            redis_key = f'{request.path}_last_task_user-{request.user.pk}'
+            if task_id := redis_cache.get(redis_key):
+                task = AsyncResult(id=task_id)
+                if task.status in ['SUCCESS', 'FAILURE', 'ERROR']:
+                    redis_cache.delete(redis_key)
+            return render(request, self.template_name, {'task': task})
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, request: WSGIRequest, *args, **kwargs):
+        """Метод изменения данных пользователя."""
+
+        if command := request.POST.get('command'):
+            command, task_id = command.split(':')
+            if command == 'STOP_TASK':
+                app.control.revoke(task_id=task_id, terminate=True)
+                return redirect(self.get_success_url())
+        else:
+            msg, task = load_data_file(request)
+            messages.add_message(request, messages.INFO, msg)
+            return render(request, self.template_name, {'task': task})
 
 
-class ServiceFNSPageView(TemplateView):
+class ServiceFNSPageView(TemplateView, BaseServicesPageView):
     """ Отображение страницы политики конфиденциальности сайта """
     template_name = "/services/FNS/fns_service.j2"
 
@@ -22,57 +51,11 @@ class ServiceFNSPageView(TemplateView):
         """Возвращаемый URL при успешном выполнении методов."""
         return reverse_lazy('services:fns_service')
 
-    def get(self, request, *args, **kwargs):
-        if request.user.groups.filter(name__in=['users', 'admins']).exists() or request.user.is_superuser:
-            task = None
-            redis_key = f'{request.path}_last_task_user-{request.user.pk}'
-            if task_id := redis_cache.get(redis_key):
-                task = AsyncResult(id=task_id)
-                if task.status in ['SUCCESS', 'FAILURE', 'ERROR']:
-                    redis_cache.delete(redis_key)
-            return render(request, self.template_name, {'task': task})
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        """Метод изменения данных пользователя."""
-        msg, task = load_data_file(request)
-        # messages.add_message(request, messages.WARNING, 'Загрузка файла, валидация данных...')
-
-        messages.add_message(self.request, messages.INFO, msg)
-        return render(
-            request,
-            self.template_name,
-            {'task': task}
-        )
-
-        # return HttpResponseRedirect(self.get_success_url(), {'task_id': task.task_id})
-
-
-class ServiceFSSPPageView(TemplateView):
+class ServiceFSSPPageView(TemplateView, BaseServicesPageView):
     """ Отображение страницы политики конфиденциальности сайта """
     template_name = "/services/FSSP/fssp_service.j2"
 
-    def get(self, request, *args, **kwargs):
-        if request.user.groups.filter(name__in=['users', 'admins']).exists() or request.user.is_superuser:
-            task = None
-            redis_key = f'{request.path}_last_task_user-{request.user.pk}'
-            if task_id := redis_cache.get(redis_key):
-                task = AsyncResult(id=task_id)
-                if task.status in ['SUCCESS', 'FAILURE', 'ERROR']:
-                    redis_cache.delete(redis_key)
-            return render(request, self.template_name, {'task': task})
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, request, *args, **kwargs):
-        """Метод изменения данных пользователя."""
-        msg, task = load_data_file(request)
-        # messages.add_message(request, messages.WARNING, 'Загрузка файла, валидация данных...')
-
-        messages.add_message(self.request, messages.INFO, msg)
-        return render(
-            request,
-            self.template_name,
-            {'task': task}
-        )
+    def get_success_url(self):
+        """Возвращаемый URL при успешном выполнении методов."""
+        return reverse_lazy('services:fssp_service')
